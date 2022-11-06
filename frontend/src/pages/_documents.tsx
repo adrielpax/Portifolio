722b4d1d0eb09928d0cceaa023899008c9e2b0d7
T4SpaX <playerx173@gmail.com>
2022-11-06 22:07:29 +0000

    featured

---
+++ frontend/src/pages/_documents.tsx
@@ -0,0 +1,5 @@
+export function Document(){
+    return(
+        <div></div>
+    )
+}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: [
     './src/**/*.tsx'
   ],
@@ -7,4 +10,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+})
