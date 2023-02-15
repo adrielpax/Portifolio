98c7b8200291bef5437ba941a12e910ac1114f6a
T4SpaX <playerx173@gmail.com>
2023-02-15 22:51:57 +0000

    fixed error in build

--- frontend/src/pages/index.page.tsx
+++ frontend/src/pages/index.page.tsx
@@ -1,8 +1,8 @@
 //import Head from 'next/head'
 //import Image from 'next/image'
 import type {ReactElement} from 'react'
-import { NextPageWithLayout } from './_app'
-import  Homepage  from './Homepage'
+import { NextPageWithLayout } from './_app.page'
+import  Homepage  from './Homepage.page'
 
 const Page: NextPageWithLayout = ()=>{
   return (
