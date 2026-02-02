3d4a2a23fe2f900e492448512519c9d6d28143cb
adrielpax <adriel.aprendiz@gmail.com>
2026-02-02 19:32:09 -0300

    impossible

--- site/src/components/layout/Profile_Card.tsx
+++ site/src/components/layout/Profile_Card.tsx
@@ -9,17 +9,17 @@ const mockLinks = [
   {
     name: "GitHub",
     url: "https://github.com/adrielpax",
-    icon: <Github className='w-5 h-5'/>
+    icon: ""
   },
   {
     name: "LinkedIn",
     url: "https://linkedin.com/in/adriel-lucas",
-    icon:  <Linkedin className='w-5 h-5'/>,
+    icon:  "",
   },
   {
     name: "Email",
     url: "mailto:adrielsilva.ext@gmail.com",
-    icon:     <Mail className='w-5 h-5'/>
+    icon: "",
   }
 ]
 
@@ -58,7 +58,7 @@ const MainCard: React.FC<MainCardProps> = ({ onOpenContact }) => {
           {/* Info */}
           <div className="flex flex-col">
             <h2 className="text-2xl text-white flex text-left items-center font-bold gap-2 mb-2"> 
-               <BadgeCheck className='text-blue-500' /> Adriel L.  </h2>
+               Adriel L.  </h2>
             <p className="text-cyan-400/60 text-left mb-4">Analista de Sistemas Web | Soluções Tecnologicas e Digitais</p>
             <p className="text-gray-200/50  text-sm leading-relaxed mb-6 text-left ">
               Especializado em interfaces modernas, automação e soluções
@@ -92,7 +92,7 @@ const MainCard: React.FC<MainCardProps> = ({ onOpenContact }) => {
             className="w-auto self-center bg-gradient-to-r text-sm from-green-500/30 to-blue-500/30 border-white/10 border-2 
             hover:from-green-600 flex items-center gap-2 hover:to-blue-600 p-2 scale-90 px-4 rounded-lg font-semibold transition-all duration-300 transform hover:scale-105 hover:shadow-lg hover:shadow-green-500/25 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-50"
           >
-            Entrar em Contato  <Send className='w-5 h-5'/>
+            Entrar em Contato 
           </button>
             </div>
           </div>
